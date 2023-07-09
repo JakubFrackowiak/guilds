e61a388f9142e05d05dfc0f5173fdbb16b6d5e4f
OliwierOstrowski <86793212+OliwierOST@users.noreply.github.com>
2023-07-09 18:02:54 +0200

    Feature: Add formatColor function (#226)
    * Add formatColor function
    
    * Return the color as hex instead rgb

--- formatters/index.tsx
+++ formatters/index.tsx
@@ -21,3 +21,14 @@ export const formatBid = (bid: Bid) => {
   const { currency, totalBidValue } = bid
   return currency == "%" ? totalBidValue + currency : currency + totalBidValue
 }
+
+export const formatColor = (percentage: number) => {
+  const minRGB = [192, 16, 72]
+  const maxRGB = [254, 163, 180]
+  const rgb = minRGB.map((color, i) => {
+    const diff = maxRGB[i] - color
+    return Math.round(maxRGB[i] - (diff * percentage) / 100)
+  })
+  const h = rgb[0] * 0x10000 + rgb[1] * 0x100 + rgb[2] * 0x1
+  return "#" + ("000000" + h.toString(16)).slice(-6)
+}
